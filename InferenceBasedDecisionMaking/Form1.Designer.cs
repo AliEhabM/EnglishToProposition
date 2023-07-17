7f54747a17015c9cd017841420a59c7f2eca0d67
AliEhabM <121381552+AliEhabM@users.noreply.github.com>
2023-07-17 16:08:12 +0300

    Add files via upload

---
+++ InferenceBasedDecisionMaking/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace InferenceBasedDecisionMaking
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ifThen = new Button();
+            orBtn = new Button();
+            andBtn = new Button();
+            EventAdd = new Button();
+            inputBox = new GroupBox();
+            InputText = new TextBox();
+            EventDropdown = new ComboBox();
+            EventCreateInp = new TextBox();
+            EventCreate = new Button();
+            EventList = new GroupBox();
+            EventListbox = new TextBox();
+            EndBtn = new Button();
+            Begin = new Button();
+            Undo = new Button();
+            clearBtn = new Button();
+            Negate = new Button();
+            translation = new GroupBox();
+            translationBox = new TextBox();
+            Conclusion = new Button();
+            inputBox.SuspendLayout();
+            EventList.SuspendLayout();
+            translation.SuspendLayout();
+            SuspendLayout();
+            // 
+            // ifThen
+            // 
+            ifThen.Location = new Point(88, 54);
+            ifThen.Name = "ifThen";
+            ifThen.Size = new Size(94, 29);
+            ifThen.TabIndex = 0;
+            ifThen.Text = "If";
+            ifThen.UseVisualStyleBackColor = true;
+            ifThen.Click += ifThen_Click;
+            // 
+            // orBtn
+            // 
+            orBtn.Enabled = false;
+            orBtn.Location = new Point(204, 54);
+            orBtn.Name = "orBtn";
+            orBtn.Size = new Size(94, 29);
+            orBtn.TabIndex = 1;
+            orBtn.Text = "OR";
+            orBtn.UseVisualStyleBackColor = true;
+            orBtn.Click += orBtn_Click;
+            // 
+            // andBtn
+            // 
+            andBtn.Enabled = false;
+            andBtn.Location = new Point(314, 54);
+            andBtn.Name = "andBtn";
+            andBtn.Size = new Size(94, 29);
+            andBtn.TabIndex = 2;
+            andBtn.Text = "AND";
+            andBtn.UseVisualStyleBackColor = true;
+            andBtn.Click += andBtn_Click;
+            // 
+            // EventAdd
+            // 
+            EventAdd.Location = new Point(283, 89);
+            EventAdd.Name = "EventAdd";
+            EventAdd.Size = new Size(94, 29);
+            EventAdd.TabIndex = 3;
+            EventAdd.Text = "Add Event";
+            EventAdd.UseVisualStyleBackColor = true;
+            EventAdd.Click += EventAdd_Click;
+            // 
+            // inputBox
+            // 
+            inputBox.Controls.Add(InputText);
+            inputBox.Location = new Point(59, 133);
+            inputBox.Name = "inputBox";
+            inputBox.Size = new Size(431, 307);
+            inputBox.TabIndex = 4;
+            inputBox.TabStop = false;
+            inputBox.Text = "Input";
+            // 
+            // InputText
+            // 
+            InputText.Location = new Point(6, 26);
+            InputText.Multiline = true;
+            InputText.Name = "InputText";
+            InputText.ReadOnly = true;
+            InputText.Size = new Size(419, 265);
+            InputText.TabIndex = 0;
+            // 
+            // EventDropdown
+            // 
+            EventDropdown.DropDownStyle = ComboBoxStyle.DropDownList;
+            EventDropdown.FormattingEnabled = true;
+            EventDropdown.Location = new Point(59, 89);
+            EventDropdown.Name = "EventDropdown";
+            EventDropdown.Size = new Size(206, 28);
+            EventDropdown.TabIndex = 5;
+            // 
+            // EventCreateInp
+            // 
+            EventCreateInp.Location = new Point(511, 19);
+            EventCreateInp.Multiline = true;
+            EventCreateInp.Name = "EventCreateInp";
+            EventCreateInp.Size = new Size(250, 64);
+            EventCreateInp.TabIndex = 6;
+            // 
+            // EventCreate
+            // 
+            EventCreate.Location = new Point(511, 89);
+            EventCreate.Name = "EventCreate";
+            EventCreate.Size = new Size(250, 29);
+            EventCreate.TabIndex = 7;
+            EventCreate.Text = "Create Event";
+            EventCreate.UseVisualStyleBackColor = true;
+            EventCreate.Click += EventCreate_Click;
+            // 
+            // EventList
+            // 
+            EventList.Controls.Add(EventListbox);
+            EventList.Location = new Point(511, 133);
+            EventList.Name = "EventList";
+            EventList.Size = new Size(250, 351);
+            EventList.TabIndex = 8;
+            EventList.TabStop = false;
+            EventList.Text = "Events";
+            // 
+            // EventListbox
+            // 
+            EventListbox.Location = new Point(6, 26);
+            EventListbox.Multiline = true;
+            EventListbox.Name = "EventListbox";
+            EventListbox.ReadOnly = true;
+            EventListbox.Size = new Size(238, 319);
+            EventListbox.TabIndex = 0;
+            // 
+            // EndBtn
+            // 
+            EndBtn.Enabled = false;
+            EndBtn.Location = new Point(88, 19);
+            EndBtn.Name = "EndBtn";
+            EndBtn.Size = new Size(94, 29);
+            EndBtn.TabIndex = 9;
+            EndBtn.Text = "End";
+            EndBtn.UseVisualStyleBackColor = true;
+            EndBtn.Click += EndBtn_Click;
+            // 
+            // Begin
+            // 
+            Begin.Enabled = false;
+            Begin.Location = new Point(59, 455);
+            Begin.Name = "Begin";
+            Begin.Size = new Size(239, 29);
+            Begin.TabIndex = 10;
+            Begin.Text = "Translate";
+            Begin.UseVisualStyleBackColor = true;
+            Begin.Click += Begin_Click;
+            // 
+            // Undo
+            // 
+            Undo.Location = new Point(204, 19);
+            Undo.Name = "Undo";
+            Undo.Size = new Size(94, 29);
+            Undo.TabIndex = 11;
+            Undo.Text = "Undo";
+            Undo.UseVisualStyleBackColor = true;
+            Undo.Click += Undo_Click;
+            // 
+            // clearBtn
+            // 
+            clearBtn.Location = new Point(304, 455);
+            clearBtn.Name = "clearBtn";
+            clearBtn.Size = new Size(186, 29);
+            clearBtn.TabIndex = 1;
+            clearBtn.Text = "Clear";
+            clearBtn.UseVisualStyleBackColor = true;
+            clearBtn.Click += clearBtn_Click;
+            // 
+            // Negate
+            // 
+            Negate.Location = new Point(396, 89);
+            Negate.Name = "Negate";
+            Negate.Size = new Size(94, 29);
+            Negate.TabIndex = 12;
+            Negate.Text = "Negate";
+            Negate.UseVisualStyleBackColor = true;
+            Negate.Click += Negate_Click;
+            // 
+            // translation
+            // 
+            translation.Controls.Add(translationBox);
+            translation.Location = new Point(767, 18);
+            translation.Name = "translation";
+            translation.Size = new Size(240, 466);
+            translation.TabIndex = 13;
+            translation.TabStop = false;
+            translation.Text = "Translation";
+            // 
+            // translationBox
+            // 
+            translationBox.Location = new Point(6, 26);
+            translationBox.Multiline = true;
+            translationBox.Name = "translationBox";
+            translationBox.ReadOnly = true;
+            translationBox.ScrollBars = ScrollBars.Horizontal;
+            translationBox.Size = new Size(228, 434);
+            translationBox.TabIndex = 0;
+            // 
+            // Conclusion
+            // 
+            Conclusion.Enabled = false;
+            Conclusion.Location = new Point(314, 19);
+            Conclusion.Name = "Conclusion";
+            Conclusion.Size = new Size(94, 29);
+            Conclusion.TabIndex = 15;
+            Conclusion.Text = "Therefore";
+            Conclusion.UseVisualStyleBackColor = true;
+            Conclusion.Click += Conclusion_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1019, 520);
+            Controls.Add(Conclusion);
+            Controls.Add(translation);
+            Controls.Add(Negate);
+            Controls.Add(clearBtn);
+            Controls.Add(Undo);
+            Controls.Add(Begin);
+            Controls.Add(EndBtn);
+            Controls.Add(EventList);
+            Controls.Add(EventCreate);
+            Controls.Add(EventCreateInp);
+            Controls.Add(EventDropdown);
+            Controls.Add(inputBox);
+            Controls.Add(EventAdd);
+            Controls.Add(andBtn);
+            Controls.Add(orBtn);
+            Controls.Add(ifThen);
+            Name = "Form1";
+            Text = "Inference-Based Project";
+            inputBox.ResumeLayout(false);
+            inputBox.PerformLayout();
+            EventList.ResumeLayout(false);
+            EventList.PerformLayout();
+            translation.ResumeLayout(false);
+            translation.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button ifThen;
+        private Button orBtn;
+        private Button andBtn;
+        private Button EventAdd;
+        private GroupBox inputBox;
+        private ComboBox EventDropdown;
+        private TextBox EventCreateInp;
+        private Button EventCreate;
+        private GroupBox EventList;
+        private Button EndBtn;
+        private Button Begin;
+        private TextBox InputText;
+        private Button Undo;
+        private Button clearBtn;
+        private TextBox EventListbox;
+        private Button Negate;
+        private GroupBox translation;
+        private TextBox translationBox;
+        private Button Conclusion;
+    }
+}
---
+++ InferenceBasedDecisionMaking/Form1.cs
@@ -0,0 +1,386 @@
+﻿using System.Collections;
+using System.Windows.Forms.VisualStyles;
+
+namespace InferenceBasedDecisionMaking
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string ReplaceFirst(string text, string search, string replace)
+        {
+            int pos = text.IndexOf(search);
+            if (pos < 0)
+            {
+                return text;
+            }
+            return text.Substring(0, pos) + replace + text.Substring(pos + search.Length);
+        }
+        string input = "";
+        bool ifFlg = true;
+        List<string> list = new List<string>();
+        List<string> events = new List<string>();
+        Dictionary<char, string> events1 = new Dictionary<char, string>();
+        string target;
+        List<string> myList = new List<string>();
+
+
+        void printList(List<string> list, TextBox box)
+        {
+            box.Text = String.Empty;
+            foreach (string s in list)
+            {
+                box.Text += s;
+            }
+        }
+
+        private void ifThen_Click(object sender, EventArgs e)
+        {
+            if (ifFlg)
+            {
+                EventAdd.Enabled = true; Negate.Enabled = true;
+                orBtn.Enabled = false;
+                andBtn.Enabled = false;
+                EndBtn.Enabled = false;
+                list.Add("If ");
+                ifFlg = false;
+                ifThen.Text = "Then";
+                printList(list, InputText);
+                events.Add("if");
+                ifThen.Enabled = false;
+            }
+            else
+            {
+                EventAdd.Enabled = true; Negate.Enabled = true;
+                orBtn.Enabled = false;
+                andBtn.Enabled = false;
+                EndBtn.Enabled = false;
+                list.Add("then ");
+                ifFlg = true;
+                ifThen.Text = "If";
+                printList(list, InputText);
+                events.Add("then");
+                ifThen.Enabled = false;
+            }
+            Conclusion.Enabled = false;
+
+
+        }
+
+        private void orBtn_Click(object sender, EventArgs e)
+        {
+            events.Add("or");
+            list.Add(" or ");
+            printList(list, InputText);
+            orBtn.Enabled = false;
+            andBtn.Enabled = false;
+            EndBtn.Enabled = false;
+            EventAdd.Enabled = true;
+            Conclusion.Enabled = false;
+        }
+
+        private void andBtn_Click(object sender, EventArgs e)
+        {
+            events.Add("and");
+            list.Add(" and ");
+            printList(list, InputText);
+            orBtn.Enabled = false;
+            andBtn.Enabled = false;
+            EndBtn.Enabled = false;
+            EventAdd.Enabled = true; Negate.Enabled = true;
+            Conclusion.Enabled = false;
+        }
+
+        private void Undo_Click(object sender, EventArgs e)
+        {
+            if (events.Count > 0) events.RemoveAt(events.Count - 1);
+            if (list.Count > 0)
+            {
+                if (list[list.Count - 1] == "then ")
+                {
+                    ifFlg = false;
+                    ifThen.Text = "Then";
+                    if (list.Count > 1) if ((list[list.Count - 2] != "then " || list[list.Count - 2] != "If "))
+                        {
+                            EventAdd.Enabled = true; Negate.Enabled = true;
+                            orBtn.Enabled = true;
+                            andBtn.Enabled = true;
+                            EndBtn.Enabled = true;
+                            ifThen.Enabled = true;
+                        }
+                }
+                if (list[list.Count - 1] == "If ")
+                {
+                    ifFlg = true;
+                    ifThen.Text = "If";
+                    if (list.Count > 1) if ((list[list.Count - 2] != "then " || list[list.Count - 2] != "If "))
+                        {
+                            EventAdd.Enabled = true; Negate.Enabled = true;
+                            orBtn.Enabled = true;
+                            andBtn.Enabled = true;
+                            EndBtn.Enabled = true;
+                            ifThen.Enabled = true;
+                        }
+
+                }
+                EventAdd.Enabled = true; Negate.Enabled = true;
+                if (Conclusion.Enabled == false)
+                {
+                    if (!list.Contains("Therefore "))
+                    {
+                        Conclusion.Enabled = true;
+                    }
+                }
+                list.RemoveAt(list.Count - 1);
+                if (list.Count == 0)
+                {
+                    ClearText();
+                }
+
+            }
+            printList(list, InputText);
+
+        }
+
+        void ClearText()
+        {
+            InputText.Text = string.Empty;
+            list.Clear();
+            events.Clear();
+            EventAdd.Enabled = true; Negate.Enabled = true;
+            orBtn.Enabled = false;
+            andBtn.Enabled = false;
+            EndBtn.Enabled = false;
+            ifThen.Enabled = true;
+            Conclusion.Enabled = false;
+            ifFlg = true;
+            ifThen.Text = "If";
+            translationBox.Text = string.Empty;
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            ClearText();
+        }
+
+        private void EventAdd_Click(object sender, EventArgs e)
+        {
+            if (EventDropdown.SelectedItem != null)
+            {
+                char ToID = Char.Parse(EventDropdown.SelectedItem.ToString());
+                list.Add(events1[ToID] + " ");
+                events.Add(ToID.ToString());
+                orBtn.Enabled = true;
+                andBtn.Enabled = true;
+                if (ifFlg)
+                {
+                    EndBtn.Enabled = true;
+                    ifThen.Enabled = false;
+                }
+                else ifThen.Enabled = true;
+                EventAdd.Enabled = false; Negate.Enabled = false;
+                printList(list, InputText);
+                Conclusion.Enabled = false;
+            }
+
+        }
+
+        private void EventCreate_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(EventCreateInp.Text))
+            {
+                char count = Char.Parse(events1.Count.ToString());
+                events1.Add(count, EventCreateInp.Text);
+                string nl = "\r\n";
+                EventListbox.Text += nl + count + ": " + EventCreateInp.Text;
+                EventDropdown.Items.Add(count);
+            }
+
+        }
+
+        private void EndBtn_Click(object sender, EventArgs e)
+        {
+            EventAdd.Enabled = true; Negate.Enabled = true;
+            orBtn.Enabled = true;
+            andBtn.Enabled = true;
+            EndBtn.Enabled = false;
+            ifThen.Enabled = true;
+            Conclusion.Enabled = true;
+            ifFlg = true;
+            ifThen.Text = "If";
+            list[list.Count - 1] = list[list.Count - 1].Remove(list[list.Count - 1].Length - 1, 1) + ". ";
+            printList(list, InputText);
+            events.Add(".");
+            Begin.Enabled = true;
+
+            if (list.Contains("Therefore "))
+            {
+                orBtn.Enabled = false;
+                andBtn.Enabled = false;
+                ifThen.Enabled = false;
+                EndBtn.Enabled = false;
+                EventAdd.Enabled = false;
+                Negate.Enabled = false;
+            }
+
+        }
+
+        private void Negate_Click(object sender, EventArgs e)
+        {
+            if (EventDropdown.SelectedItem != null)
+            {
+                char ToID = Char.Parse(EventDropdown.SelectedItem.ToString());
+                string Event = events1[ToID];
+                if (Event.Contains(" do "))
+
+                    Event = ReplaceFirst(Event, " do ", " do not ");
+
+                else if (Event.Contains(" will "))
+
+                    Event = ReplaceFirst(Event, " will ", " will not ");
+
+                else if (Event.Contains(" can "))
+
+                    Event = ReplaceFirst(Event, " can ", " can not ");
+
+                else if (Event.Contains(" is "))
+
+                    Event = ReplaceFirst(Event, " is ", " is not ");
+
+                else if (Event.Contains(" are "))
+
+                    Event = ReplaceFirst(Event, " are ", " are not ");
+
+                else if (Event.Contains(" am "))
+
+                    Event = ReplaceFirst(Event, " am ", " am not ");
+
+                else if (Event.Contains(" "))
+
+                    Event = ReplaceFirst(Event, " ", " do not ");
+
+                else
+                {
+                    Event = "Not " + Event;
+                }
+                list.Add(Event + " ");
+                events.Add("!" + ToID.ToString());
+
+                orBtn.Enabled = true;
+                andBtn.Enabled = true;
+                if (ifFlg)
+                {
+                    EndBtn.Enabled = true;
+                    ifThen.Enabled = false;
+                }
+                else ifThen.Enabled = true;
+                EventAdd.Enabled = false; Negate.Enabled = false;
+                printList(list, InputText);
+                Conclusion.Enabled = false;
+            }
+
+        }
+
+        private void Begin_Click(object sender, EventArgs e)
+        {
+            translationBox.Text = string.Empty;
+            TranslateInput();
+            int lastLine = myList.Count - 1;
+            target = myList[lastLine].ToString();
+        }
+
+        void TranslateInput()
+        {
+            bool thenFlag = false;
+            string temp;
+            for (int i = 0; i < events.Count; i++)
+            {
+                switch (events[i])
+                {
+                    case ".":
+                        if (thenFlag) input += ")";
+                        input += "\r\n";
+                        myList.Add(input);
+                        input = "";
+                        break;
+                    case "if":
+                        input += "(";
+                        thenFlag = true;
+                        break;
+                    case "then":
+                        input += ")→";
+                        thenFlag = false;
+                        break;
+                    case "or":
+                        input += "∨";
+                        break;
+                    case "and":
+                        input += "∧";
+                        break;
+                    case "conc":
+                        input += "=====================" + "\r\n";
+                        break;
+                    default:
+                        temp = events[i];
+                        if (events[i].Contains("!"))
+                        {
+                            temp = events[i].Remove(0, 1);
+                            input += "¬";
+                        }
+
+                        input += temp;
+                        break;
+                }
+
+
+            }
+            printList(myList, translationBox);
+        }
+
+
+        private void Conclusion_Click(object sender, EventArgs e)
+        {
+            list.Add("Therefore ");
+            Conclusion.Enabled = false;
+            printList(list, InputText);
+            EventAdd.Enabled = true;
+            Negate.Enabled = true;
+            orBtn.Enabled = true;
+            andBtn.Enabled = true;
+            EndBtn.Enabled = false;
+            Begin.Enabled = false;
+            ifThen.Enabled = true;
+            events.Add("conc");
+        }
+
+
+
+        string implies(string arg1, string arg2)
+        {
+            return "(" + (arg1) + ")→(" + arg2 + ")";
+        }
+
+        string or(string arg1, string arg2)
+        {
+            return "(" + (arg1) + "∨" + arg2 + ")";
+        }
+
+        string and(string arg1, string arg2)
+        {
+            return "(" + (arg1) + "∧" + arg2 + ")";
+        }
+
+        string not(string arg)
+        {
+            if (!arg.Contains("¬"))
+                return "¬(" + arg + ")";
+            else return arg.Substring(1);
+        }
+
+
+    }
+}
